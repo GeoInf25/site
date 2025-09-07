27659e61c8a2f617220bd6c2e029d42985bd15b9
GeoInf25 <michelotti002@gmail.com>
2025-09-07 21:19:02 +0200

    Add files via upload

---
+++ CSharp/App001/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace App001 {
+    partial class Form1 {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_OFF = new System.Windows.Forms.Button();
+            this.btn_ON = new System.Windows.Forms.Button();
+            this.reoGridControl1 = new unvell.ReoGrid.ReoGridControl();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_latLngZoom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1084, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1084, 437);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.reoGridControl1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(758, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(326, 437);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_OFF);
+            this.panel1.Controls.Add(this.btn_ON);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(326, 40);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADD MARKER ON MAP:";
+            // 
+            // btn_OFF
+            // 
+            this.btn_OFF.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_OFF.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_OFF.Location = new System.Drawing.Point(192, 4);
+            this.btn_OFF.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_OFF.Name = "btn_OFF";
+            this.btn_OFF.Size = new System.Drawing.Size(40, 28);
+            this.btn_OFF.TabIndex = 1;
+            this.btn_OFF.Text = "OFF";
+            this.btn_OFF.UseVisualStyleBackColor = false;
+            // 
+            // btn_ON
+            // 
+            this.btn_ON.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_ON.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ON.Location = new System.Drawing.Point(152, 4);
+            this.btn_ON.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_ON.Name = "btn_ON";
+            this.btn_ON.Size = new System.Drawing.Size(40, 28);
+            this.btn_ON.TabIndex = 0;
+            this.btn_ON.Text = "ON";
+            this.btn_ON.UseVisualStyleBackColor = false;
+            // 
+            // reoGridControl1
+            // 
+            this.reoGridControl1.BackColor = System.Drawing.Color.White;
+            this.reoGridControl1.ColumnHeaderContextMenuStrip = null;
+            this.reoGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reoGridControl1.LeadHeaderContextMenuStrip = null;
+            this.reoGridControl1.Location = new System.Drawing.Point(3, 43);
+            this.reoGridControl1.Name = "reoGridControl1";
+            this.reoGridControl1.RowHeaderContextMenuStrip = null;
+            this.reoGridControl1.Script = null;
+            this.reoGridControl1.SheetTabContextMenuStrip = null;
+            this.reoGridControl1.SheetTabNewButtonVisible = true;
+            this.reoGridControl1.SheetTabVisible = true;
+            this.reoGridControl1.SheetTabWidth = 60;
+            this.reoGridControl1.ShowScrollEndSpacing = true;
+            this.reoGridControl1.Size = new System.Drawing.Size(320, 391);
+            this.reoGridControl1.TabIndex = 1;
+            this.reoGridControl1.Text = "reoGridControl1";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.webView21, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(758, 437);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 40);
+            this.webView21.Margin = new System.Windows.Forms.Padding(0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(758, 437);
+            this.webView21.TabIndex = 2;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txt_latLngZoom);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(758, 40);
+            this.panel2.TabIndex = 0;
+            // 
+            // txt_latLngZoom
+            // 
+            this.txt_latLngZoom.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_latLngZoom.Location = new System.Drawing.Point(164, 10);
+            this.txt_latLngZoom.Name = "txt_latLngZoom";
+            this.txt_latLngZoom.Size = new System.Drawing.Size(150, 23);
+            this.txt_latLngZoom.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Latitude, longitude, zoom:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 461);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "App001";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private unvell.ReoGrid.ReoGridControl reoGridControl1;
+        private System.Windows.Forms.Button btn_OFF;
+        private System.Windows.Forms.Button btn_ON;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Panel panel2;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.TextBox txt_latLngZoom;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CSharp/App001/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+using unvell.ReoGrid;
+using System.Globalization;
+using unvell.ReoGrid.DataFormat;
+
+namespace App001
+{
+    
+    public partial class Form1 : Form {
+
+        private AutoResetEvent navigationCompleted;
+        private string lat;
+        private string lng;
+        private string latClick;
+        private string lngClick;
+        private int zoom;
+        private dynamic sheet1;
+        private int row;
+        private int numberMarker; 
+
+        public Form1() {
+            InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            this.Icon = new Icon(SystemIcons.Shield, 40, 40);
+            lat = "42.810000";
+            lng = "10.330000";
+            zoom = 15;
+            txt_latLngZoom.Text = "" + lat + " :: " + lng + " :: " + zoom;
+            sheet1 = reoGridControl1.Worksheets[0];
+            row = 1; 
+        }
+
+        private void print(string arg) {
+            MessageBox.Show("" + arg);
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e) {
+            MessageBox.Show("App001 -- March 2025 -- Version 1.0",  "Info");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e) {
+
+            navigationCompleted = new AutoResetEvent(false);
+
+            string pathPro = "" + Directory.GetParent("" + Environment.CurrentDirectory).Parent.FullName;
+            await webView21.EnsureCoreWebView2Async();
+
+            //event
+            webView21.NavigationCompleted += (_, args) => { navigationCompleted.Set(); };
+            webView21.CoreWebView2.Navigate(pathPro + @"\webPage.html");
+            await Task.Run(() => { navigationCompleted.WaitOne(); });
+
+            await webView21.ExecuteScriptAsync(
+                @"var map = L.map('map').setView([ " + lat + ", " + lng + " ], " + zoom + "); " +
+                @"L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
+                    .addTo(map);"
+            );
+
+            //event
+            webView21.WebMessageReceived += (_, args) => {
+                string[] temp = args.TryGetWebMessageAsString().Split('@');
+                switch (temp[0]) {
+                    case "mouseMove":
+                        lat = temp[1];
+                        lng = temp[2];
+                        break;
+                    case "zoomEnd":
+                        zoom = Convert.ToInt32(temp[1]);
+                        break;
+                    case "click":
+                        latClick = temp[1];
+                        lngClick = temp[2];
+                        numberMarker = Convert.ToInt32(temp[3]);
+                        sheet1[row, 0] = "Marker" + numberMarker;
+                        sheet1[row, 1] = "" + latClick;
+                        sheet1[row, 2] = "" + lngClick;
+                        row++;
+                        break;
+                }
+                txt_latLngZoom.Text = "" + lat + " :: " + lng + " :: " + zoom;
+
+            };
+
+            await webView21.ExecuteScriptAsync(
+                "map.on('mousemove', function( e ) { chrome.webview.postMessage( '' + [ 'mouseMove' , e.latlng.lat.toFixed(6) , e.latlng.lng.toFixed(6) ].join( '@' ) ); });" +
+                "map.on('zoomend', function( e ) { chrome.webview.postMessage( '' + [ 'zoomEnd' , map.getZoom() ].join( '@' ) );  });"
+            );
+
+            reoGridControl1.SheetTabWidth = 200;
+            sheet1.Name = "MARKERS";
+
+            sheet1[0, 0] = "";
+            sheet1[0, 1] = "Latitude";
+            sheet1[0, 2] = "Longitude";
+
+            sheet1.SetRangeDataFormat(RangePosition.EntireRange, CellDataFormatFlag.Number,
+            new NumberDataFormatter.NumberFormatArgs() {
+                DecimalPlaces = 6,
+                //UseSeparator = true,
+            });
+
+            sheet1.SetRangeStyles(RangePosition.EntireRange, new WorksheetRangeStyle() {
+                Flag = PlainStyleFlag.FontSize | PlainStyleFlag.FontName,
+                FontName = "Verdana",
+                FontSize = 9,
+            });
+
+            await webView21.ExecuteScriptAsync(
+                "var markers = [];" + 
+                "var insertMarker = false; " + 
+                "var count = 0"
+            );
+
+            await webView21.ExecuteScriptAsync(
+                    "map.on( 'click' , function(e) { " +
+                    "   if( insertMarker == true ) {" +
+                    "       tempLat = e.latlng.lat.toFixed(6); " +
+                    "       tempLng = e.latlng.lng.toFixed(6); " +
+                    "       count++; " +
+                    "       var tempMarker = L.marker([ tempLat , tempLng ]).bindPopup(\"I'm Marker\" + count + \" - coord:\" + tempLat + \"::\" + tempLng );" +
+                    "       markers.push( tempMarker ); " +
+                    "       map.addLayer( markers[ count - 1 ] ); " +
+                    "       chrome.webview.postMessage('' + ['click', tempLat, tempLng, count ].join('@'));" +
+                    "   }" +
+                    "});"
+                );
+
+            btn_ON.Click += async (_, args) => {
+                btn_ON.BackColor =  Color.FromArgb( 255, 192, 255, 192 );
+                await webView21.ExecuteScriptAsync(
+                    "insertMarker = true; "
+                );
+                
+            };
+
+            btn_OFF.Click += async (_, args) => {
+                btn_ON.BackColor = SystemColors.ControlLightLight;
+                await webView21.ExecuteScriptAsync(
+                    "insertMarker = false; " + 
+                    "for( var i = 0; i < markers.length; i++ ) {" +
+                    "   map.removeLayer( markers[ i ] )" + 
+                    "}" + 
+                    "markers = [];" + 
+                    "count = 0"
+                );
+                
+                sheet1.Reset(200, 200);
+                sheet1[0, 0] = "";
+                sheet1[0, 1] = "Latitude";
+                sheet1[0, 2] = "Longitude";
+                row = 1;
+
+                sheet1.SetRangeDataFormat(RangePosition.EntireRange, CellDataFormatFlag.Number,
+                    new NumberDataFormatter.NumberFormatArgs() {
+                    DecimalPlaces = 6,
+                    //UseSeparator = true,
+                });
+
+            };
+
+        }
+
+        
+    }    
+}
+
