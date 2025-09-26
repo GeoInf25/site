7796be34a25f150bade638a2036a2758f659c390
GeoInf25 <michelotti002@gmail.com>
2025-09-26 19:39:19 +0200

    Update file_funzSupp.js

--- file_funzSupp.js
+++ file_funzSupp.js
@@ -1,21 +1,5 @@
 
 function gestioneSchede( scheda ) {
-	
-	var schedaPython = document.getElementById( "schedaPython" ); 
-	var schedaCSharp = document.getElementById( "schedaCSharp" ); 
-	var schedaJavascript = document.getElementById( "schedaJavascript" ); 
-	var schedaDatabase = document.getElementById( "schedaDatabase" );
-	var schedaDisegni3D = document.getElementById( "schedaDisegni3D" );
-	var schedaAltriLinks = document.getElementById( "schedaAltriLinks" ); 
-
-	var schede = [ 
-		schedaPython, 
-		schedaCSharp, 
-		schedaJavascript,
-		schedaDatabase,
-		schedaDisegni3D,
-		schedaAltriLinks
-	]; 
 
 	for( var i = 0; i < schede.length; i++ ) {
 		var tempNomeScheda = schede[ i ].id; 
