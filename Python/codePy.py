5320b2210178454daa2e0f96cedb3e80b39fee34
GeoInf25 <michelotti002@gmail.com>
2025-10-05 20:02:11 +0200

    Update codePy.py

--- Python/codePy.py
+++ Python/codePy.py
@@ -35,71 +35,80 @@ piano = [ -1, 0, 1, 2, 3, 4, 5 ]
 tipo = [ "Aula", "Laboratorio", "Palestra" ]
   
 def tablePopulating( *args ):
-  global connection
-  totIDTabStudent = int( document.getElementById( "totIDTabStudent" ).value )
-  totIDTabClass = int( document.getElementById( "totIDTabClass" ).value )
-  #connection = sqlite3.connect( ":memory:" ) #https://geoinf25.github.io/site/Database/dbSqliteEs01.db
-  print("connection.total_changes: " + str( connection.total_changes ) )
-  cursor = connection.cursor()
+  try: 
+    global connection
+    totIDTabStudent = int( document.getElementById( "totIDTabStudent" ).value )
+    totIDTabClass = int( document.getElementById( "totIDTabClass" ).value )
+    #connection = sqlite3.connect( ":memory:" ) #https://geoinf25.github.io/site/Database/dbSqliteEs01.db
+    print("connection.total_changes: " + str( connection.total_changes ) )
+    cursor = connection.cursor()
 
-  cursor.execute("DROP TABLE IF EXISTS Studente");
-  cursor.execute("DROP TABLE IF EXISTS Classe");
+    cursor.execute("DROP TABLE IF EXISTS Studente");
+    cursor.execute("DROP TABLE IF EXISTS Classe");
 
-  cursor.execute("CREATE TABLE IF NOT EXISTS Studente (idStud INTEGER, nome TEXT, cognome TEXT, eta INTEGER, luogoNascita TEXT, idClasse INTEGER )")
-  cursor.execute("CREATE TABLE IF NOT EXISTS Classe (idClass INTEGER, superficie INTEGER, piano INTEGER, tipo TEXT )")
+    cursor.execute("CREATE TABLE IF NOT EXISTS Studente (idStud INTEGER, nome TEXT, cognome TEXT, eta INTEGER, luogoNascita TEXT, idClasse INTEGER )")
+    cursor.execute("CREATE TABLE IF NOT EXISTS Classe (idClass INTEGER, superficie INTEGER, piano INTEGER, tipo TEXT )")
 
-  idStudente = 0
-  idClasse = 0
+    idStudente = 0
+    idClasse = 0
 
-  #print( nome[ randint(0, len(nome)-1) ] )
+    #print( nome[ randint(0, len(nome)-1) ] )
 
-  for i in range(0, totIDTabStudent, 1): #limite escluso 
-    cursor.execute("INSERT INTO Studente VALUES ( ?, ?, ?, ?, ?, ? )", ( 
-      idStudente, nome[ randint(0, len(nome)-1) ], 
-      cognome[ randint(0, len(cognome)-1) ], 
-      randint(18, 45), 
-      luogoNascita[ randint(0, len(luogoNascita)-1) ],
-      randint(0, totIDTabClass )
-    ))
-    idStudente = idStudente + 1
+    for i in range(0, totIDTabStudent, 1): #limite escluso 
+      cursor.execute("INSERT INTO Studente VALUES ( ?, ?, ?, ?, ?, ? )", ( 
+        idStudente, nome[ randint(0, len(nome)-1) ], 
+        cognome[ randint(0, len(cognome)-1) ], 
+        randint(18, 45), 
+        luogoNascita[ randint(0, len(luogoNascita)-1) ],
+        randint(0, totIDTabClass )
+      ))
+      idStudente = idStudente + 1
 
-  for i in range(0, totIDTabClass, 1): #limite escluso 
-    cursor.execute("INSERT INTO Classe VALUES ( ?, ?, ?, ? )", ( 
-      idClasse, 
-      superficie[ randint(0, len(superficie)-1) ], 
-      piano[ randint(0, len(piano)-1) ], 
-      tipo[ randint(0, len(tipo)-1) ]
-    ))
-    idClasse = idClasse + 1
+    for i in range(0, totIDTabClass, 1): #limite escluso 
+      cursor.execute("INSERT INTO Classe VALUES ( ?, ?, ?, ? )", ( 
+        idClasse, 
+        superficie[ randint(0, len(superficie)-1) ], 
+        piano[ randint(0, len(piano)-1) ], 
+        tipo[ randint(0, len(tipo)-1) ]
+      ))
+      idClasse = idClasse + 1
 
-  connection.commit()
+    connection.commit()
 
-  cursor.execute( "SELECT * FROM Studente" )
-  tempResultQuerySQL = from_db_cursor(cursor)
-  document.getElementById("txt_displayTabStudentDbEs01").innerHTML = tempResultQuerySQL.get_string()
+    cursor.execute( "SELECT * FROM Studente" )
+    tempResultQuerySQL = from_db_cursor(cursor)
+    document.getElementById("txt_displayTabStudentDbEs01").innerHTML = tempResultQuerySQL.get_string()
 
-  cursor.execute( "SELECT * FROM Classe" )
-  tempResultQuerySQL = from_db_cursor(cursor)
-  document.getElementById("txt_displayTabClassDbEs01").innerHTML = tempResultQuerySQL.get_string()
+    cursor.execute( "SELECT * FROM Classe" )
+    tempResultQuerySQL = from_db_cursor(cursor)
+    document.getElementById("txt_displayTabClassDbEs01").innerHTML = tempResultQuerySQL.get_string()
 
-  #connection.close()
-  print( "-- Fine valorizzazione Tabelle -- " )
+    #connection.close()
+    print( "-- Fine valorizzazione Tabelle -- " )
+
+  except Exception as e:
+    document.getElementById("txt_displayTabStudentDbEs01").innerHTML = f"Error detected: {str(e)}"
+    document.getElementById("txt_displayTabClassDbEs01").innerHTML = "--"
 
 def executeQuerySQL( *args ):
-  global connection
-  #print( document.getElementById("txt_displayQuerySQLDbEs01").value )
-  #connection = sqlite3.connect( ":memory:" ) #https://geoinf25.github.io/site/Database/dbSqliteEs01.db
-  cursor = connection.cursor()
-  cursor.execute( document.getElementById("txt_displayQuerySQLDbEs01").value )
-  tempResultQuerySQL = from_db_cursor(cursor)
-  document.getElementById("txt_displayResultDbEs01").innerHTML = tempResultQuerySQL.get_string()
-  #connection.close()
-  print( "-- Fine Esecuzione Query SQL -- " )
+  try: 
+    global connection
+    #print( document.getElementById("txt_displayQuerySQLDbEs01").value )
+    #connection = sqlite3.connect( ":memory:" ) #https://geoinf25.github.io/site/Database/dbSqliteEs01.db
+    cursor = connection.cursor()
+    cursor.execute( document.getElementById("txt_displayQuerySQLDbEs01").value )
+    tempResultQuerySQL = from_db_cursor(cursor)
+    document.getElementById("txt_displayResultDbEs01").innerHTML = tempResultQuerySQL.get_string()
+    #connection.close()
+    print( "-- Fine Esecuzione Query SQL -- " )
 
-#Test iniziale all'apertura della Pagina Web
-#if( window.sessionStorage.getItem( "superatoPrimoAvvio2" ) is None ):
-#document.getElementById("btn_tablePopulating").click(); 
-#window.sessionStorage.setItem( "superatoPrimoAvvio2" , 1 ) 
+    #Test iniziale all'apertura della Pagina Web
+    #if( window.sessionStorage.getItem( "superatoPrimoAvvio2" ) is None ):
+    #document.getElementById("btn_tablePopulating").click(); 
+    #window.sessionStorage.setItem( "superatoPrimoAvvio2" , 1 ) 
+
+  except Exception as e:
+    document.getElementById("txt_displayResultDbEs01").innerHTML = f"Error detected: {str(e)}"
 
 add_event_listener( document.getElementById("btn_tablePopulating") , "click", tablePopulating)
 add_event_listener( document.getElementById("btn_executeQuerySQL") , "click", executeQuerySQL)	
