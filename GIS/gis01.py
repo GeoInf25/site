e1152c56032078b90a162d5ba489e975fa7b446b
GeoInf25 <michelotti002@gmail.com>
2025-10-14 22:25:56 +0200

    Update gis01.py

--- GIS/gis01.py
+++ GIS/gis01.py
@@ -2,20 +2,34 @@
 # __all__ necessario 
 # __all_ = [ "variable" ] #Esportazione variabili
 
-import folium, pandas as pd 
+import folium
+import sqlite3
+import pandas as pd 
 from pyscript import display
 
 from js import console, document, window
 import js
 
-m = folium.Map(location=[48, -102], zoom_start=3 )
+#ufo_data = pd.read_csv( "UfoDataset.csv" , names=column_names, engine='python' )
 
-column_names = ["datetime", "city", "state", "country", "shape", "duration_seconds", "duration_text", "description", "report_date", "latitude", "longitude" ]
+m = folium.Map(location=[48, -102], zoom_start=2 )
 
 #fetch pyscript
 try: 
-    print( "Test")
-    #ufo_data = pd.read_csv( "UfoDataset.csv" , names=column_names, engine='python' )
+    connection = sqlite3.connect("UfoDb.db")
+    tempQuery = "SELECT * FROM Ufo where year = '1949' "
+
+    dfRes = pd.read_sql( sql = tempQuery , con = connection ) 
+
+    for row in range( 0 , len( dfRes ) , 1 ):
+        folium.Marker(
+            location=[ dfRes.at[ row, "latitude" ] , dfRes.at[ row, "longitude" ] ],
+            #tooltip="Click me!",
+            popup= folium.Popup( str( dfRes.at[ row, "shape" ] ).upper() + " | " + str( dfRes.at[ row, "description" ] ), max_width=200 ), 
+            #icon=folium.Icon(icon="cloud"),
+        ).add_to(m)
+
+    
 except Exception as e:
     print( f"Error detected: {str(e)}" )
 
