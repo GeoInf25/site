7f49bf2170565963995556f185f4e6ca83160c21
GeoInf25 <michelotti002@gmail.com>
2025-10-18 00:13:31 +0200

    Update foliumJS.js

--- GIS/foliumJS.js
+++ GIS/foliumJS.js
@@ -1,13 +1,28 @@
 
-function addFoliumFoo( map ) {
-    map.on("click", eventClick);
+function addFoliumFunc( map ) {
+
+    // Iframe Folium Map
+    var outside = parent.document; 
+
+    map.on( "click" , eventClick);
+    map.on( "mousemove", eventMouseMove ); 
 
     function eventClick(e) {
-        const marker = new L.marker(e.latlng, {
-        draggable: false,
-        }).addTo( map );
+        //const marker = new L.marker(e.latlng, {
+            //draggable: false,
+        //}) //.addTo( map );
+        outside.getElementById( "consoleFoliumMap" ).value += "Click on Folium Map on " + e.latlng.lat + ", " + e.latlng.lng + ".\n";
 
-        //map.setView([0, 0], 0);
+
+        //console.log( "" + parent.testArray )
+    }
+
+    function eventMouseMove( e ) {
+        outside.getElementById( "txt_latAddFoliumFunc" ).value = ( e.latlng.lat ).toFixed(6) + "°"; 
+        outside.getElementById( "txt_lngAddFoliumFunc" ).value = ( e.latlng.lng ).toFixed(6) + "°"; 
+        outside.getElementById( "txt_zoomAddFoliumFunc" ).value = map.getZoom() + ""; 
 
     }
+
+    //console.log( outside.getElementById( "txt_latAddFoliumFunc" ).value = "0.12" )
 }
