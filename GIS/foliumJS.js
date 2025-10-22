6160937f5eee07d731477dcbdb1cd54b8d15671e
GeoInf25 <michelotti002@gmail.com>
2025-10-22 23:15:07 +0200

    Update foliumJS.js

--- GIS/foliumJS.js
+++ GIS/foliumJS.js
@@ -1,8 +1,54 @@
 
-function addFoliumFunc( map ) {
+var testIframe = "testIframe"
+
+var map = null; 
+
+//var LC = null; 
+
+//var markers = null; 
+/*
+function addLC() {
+    try {
+        LC = L.control.layers( ).addTo(map);
+    } catch( e ) {
+        console.log( "" + e ); 
+    }
+}
+*/
+/*
+function removeLC( LC ) {
+    try {
+        if( LC != undefined && LC != null ) {
+            console.log( LC )
+            LC.remove(  ); 
+        }
+    } catch( e ) {
+        console.log( "" + e ); 
+    }
+}
+*/
+
+function addMarker(  ) {
+    //console.log( "AAA" + map )
+    
+    var marker = L.marker([51.5, -0.09] ).addTo(map);
+    /*
+    markers.addLayer(marker);
+    map.addLayer(markers);
+    */
+
+    console.log( "Here" )
+    
+   //var marker = L.marker([51.5, -0.09] ).addTo(map);
+    //map.setView([43.64701, -79.39425], 15);
+}
+
+function addFoliumFunc( argMap ) {
+
+    map = argMap;
 
     // Iframe Folium Map
-    var outside = parent.document; 
+    var outside = parent.document;
 
     map.on( "click" , eventClick);
     map.on( "mousemove", eventMouseMove ); 
@@ -25,4 +71,16 @@ function addFoliumFunc( map ) {
     }
 
     //console.log( outside.getElementById( "txt_latAddFoliumFunc" ).value = "0.12" )
+
+    //console.log( "::: " + outside.getElementById("foliumMap").lastChild );
+
+    //Stato Ready 
+
+    //console.log( "AAA " + outside.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow ); // uguale a 
+    //Fig  o DIV - Iframe - poi mappa 
+    //console.log( $(this)[ 0 ] ) // Risposta JQuery che contiene Window ovvero Iframe 
+
+    //console.log( "AAA " + outside.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.devicePixelRatio );
+
+
 }
