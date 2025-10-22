b70b4d78fa7d6e864b0465ab8a85a8cf659b8d4d
GeoInf25 <michelotti002@gmail.com>
2025-10-22 23:15:05 +0200

    Update gis01.py

--- GIS/gis01.py
+++ GIS/gis01.py
@@ -7,6 +7,7 @@ from folium.plugins import MousePosition
 from folium.plugins import Draw
 from folium.plugins import MeasureControl
 from folium.plugins import MiniMap
+from folium.plugins import MarkerCluster
 
 from folium import plugins
 
@@ -28,7 +29,7 @@ from random import randint
 #ufo_data = pd.read_csv( "UfoDataset.csv" , names=column_names, engine='python' )
 
 fig = folium.Figure(width="100%", height="100%")
-m = folium.Map( location=[48, -102], zoom_start=2 ).add_to( fig )
+m = folium.Map( location=[48, -102], zoom_start=2 , control_scale = True ).add_to( fig )
 
 formatter = "function(num) {return L.Util.formatNum(num, 4) + ' º ';};"
 
@@ -54,10 +55,6 @@ else:
 
 MiniMap(toggle_display=True , width=100, height=100 ).add_to(m)
 
-#folium.TileLayer('Stamen Terrain').add_to(m)
-
-folium.LayerControl().add_to(m)
-
 document.getElementById("txt_queryUfoDb").value = "select * from Ufo where year ='1949'"
 
 connection = None
@@ -70,13 +67,61 @@ ff = folium.Marker(
 """
 
 #cont = 0
+#livello = 450
 
-colorIconFolium = [ 'black', 'beige', 'lightgray', 'lightblue', 'blue', 'cadetblue', 'lightgreen', 'lightred', 'orange', 'white', 'darkblue', 'gray', 'pink', 'green', 'darkpurple', 'red', 'purple', 'darkred', 'darkgreen' ]
+#avoid color: lightgray , white
+
+colorIconFolium = [ 'black', 'beige', 'lightblue', 'blue', 'cadetblue', 'lightgreen', 'lightred', 'orange', 'darkblue', 'gray', 'pink', 'green', 'darkpurple', 'red', 'purple', 'darkred', 'darkgreen' ]
+
+
+#marker_cluster = MarkerCluster( name = "MC" )
+"""
+folium.Marker(
+    location=[40.67, -73.94],
+    popup="Add popup text here.",
+    #icon=folium.Icon(color="green", icon="ok-sign"),
+).add_to(marker_cluster)
+
+folium.Marker(
+    location=[44.67, -73.94],
+    popup="Add popup text here.",
+    #icon=folium.Icon(color="red", icon="remove-sign"),
+).add_to(marker_cluster)
+"""
+#marker_cluster.add_to( m )
+
+#folium.TileLayer('Stamen Terrain').add_to(m)
+
+"""
+folium.Marker(
+    location=[40.67, -73.94],
+    popup="Add popup text here.",
+    icon=folium.Icon(color="green", icon="ok-sign"),
+).add_to(marker_cluster)
+
+folium.Marker(
+    location=[44.67, -73.94],
+    popup="Add popup text here.",
+    icon=folium.Icon(color="red", icon="remove-sign"),
+).add_to(marker_cluster)
+
+folium.Marker(
+    location=[44.67, -71.94],
+    popup="Add popup text here.",
+    icon=None,
+).add_to(marker_cluster)
+"""
+
+#folium.LayerControl( autoZIndex = True ).add_to(m)
+#LC = folium.LayerControl()
 
 def executeQueryUfoDbFromField( *args ):
     try:
         #global fig
         #global  cont
+
+        #global livello
+        #global LC
 
         global colorIconFolium
 
@@ -96,21 +141,38 @@ def executeQueryUfoDbFromField( *args ):
         if( not( dfRes is None ) ):
 
             tempIndexColor = randint( 0, len( colorIconFolium ) - 1 )
-            
+
+            nameLayer = str( dfRes.at[ 0, "year" ] )
+
+            #tempPanel = folium.map.CustomPane( nameLayer, z_index=livello ).add_to( m )
+            #tempMC = MarkerCluster( name = nameLayer, options={ 'clusterPane': tempPanel } )
+            tempMC = MarkerCluster( name = nameLayer )
+            #livello = livello + 10
+
             for row in range( 0 , len( dfRes ) , 1 ):
                 #print( dfRes.at[ row, "latitude" ] )
-                folium.Marker(
+                tempM = folium.Marker(
                     location=[ dfRes.at[ row, "latitude" ] , dfRes.at[ row, "longitude" ] ],
                     #tooltip="Click me!",
-                    popup= folium.Popup( "DATE: " + str( dfRes.at[ row, "date" ] ) + " | SHAPE: " + str( dfRes.at[ row, "shape" ] ).upper() + " | DESCRIPTION: " + str( dfRes.at[ row, "description" ] ), max_width=200 ), 
+                    popup= folium.Popup( 
+                        "LOCATION: " + str( dfRes.at[ row, "latitude" ] ) + ", " + str( dfRes.at[ row, "longitude" ] ) + "<br>" +
+                        "DATE: " + str( dfRes.at[ row, "date" ] ) + "<br>" +
+                        "SHAPE: " + str( dfRes.at[ row, "shape" ] ).upper() + "<br>" +
+                        "DESCRIPTION: " + str( dfRes.at[ row, "description" ] 
+                    ), max_width=200 ), 
                     #icon=folium.Icon(icon="cloud"),
                     icon=folium.Icon(color= colorIconFolium[ tempIndexColor ], icon='info-sign')
-                ).add_to(m)
+                ) #.add_to( m )
+                
+                tempMC.add_child( tempM ) 
+            
+            
+            #tempMC.add_to( tempPanel )
+            
+            #location=dfRes[["latitude","longitude"]]
+            #MarkerCluster(location).add_to(m)
             
             """
-            location=dfRes[["latitude","longitude"]]
-            plugins.MarkerCluster(location).add_to(m)
-            
             ff = folium.Marker(
                 location=[ 36.0 , -106.0 ],
             )
@@ -126,12 +188,64 @@ def executeQueryUfoDbFromField( *args ):
             pass
         """
 
+        #global marker_cluster
+
+        #tempFG = folium.FeatureGroup(name= "FG_" + nameLayer, control=True).add_to(m)
+
+        tempMC.add_to( m )
+
+        #document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.removeLC( ) 
+        #document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.addLC( ) 
+
+        #LC.reset()
+        #print( LC.get_name() )
+        #LC.add_to( m )
+        #document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.removeLC( "{LC}".replace("{LC}", LC.get_name() ) ) 
+        
+
+        #m.keep_in_front( tempMC )
+
+        #icon2 = folium.Icon(color= colorIconFolium[ tempIndexColor ], icon='info-sign')
+        #document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.addMarker( ) 
+
+        #console.log( "ZZZZZ " + document.getElementById( "foliumMap" ).lastChild.childNodes.length )
+        """
         document.getElementById("foliumMap").remove();
         div = document.createElement("div");
         div.id = "foliumMap" 
         document.getElementById("contFoliumMap").appendChild(div);
 
         display( fig, target="foliumMap")
+        """
+
+        #document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.location.href = document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.location.href
+        display( fig, target="foliumMap")
+        document.getElementById( "foliumMap" ).removeChild( document.getElementById( "foliumMap" ).firstChild )
+        #document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.location.reload()
+
+        #document.getElementById( "foliumMap" ).replaceChild( fig , document.getElementById( "foliumMap" ).childNodes[0] ) #error
+
+        #m.get_root().header.add_child(folium.CssLink('https://unpkg.com/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css'))
+        #m.get_root().header.add_child(folium.JavascriptLink('https://unpkg.com/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.min.js'))
+
+        #custom_js = f"""
+        #    console.log( 'EEE ' + {m.get_name()} )
+        #"""
+
+        #m.get_root().script.add_child(folium.Element(custom_js))
+
+        #script = document.createElement('script')
+        #script.innerHTML =  "console.log( 'UUU' )"
+        #document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow.document.body.appendChild(script);
+
+        #console.log( "" + str( m.get_root().render() ) )
+
+
+        #console.log( document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow )
+        
+        #print( "DOPO : " + str( document.getElementById( "foliumMap" ).childNodes.length ) )
+
+        #console.log( document.getElementById( "foliumMap" ).lastChild.lastChild.contentWindow )
 
         print( "Finish ... ")
 
@@ -182,21 +296,26 @@ print("Shape of the DataFrame:", ufo_data.shape)"
 
 nameFoliumMap = m.get_name()
 
-m.get_root().html.add_child(folium.Element("""
-    <script type="text/javascript">
-                                            
-    $.getScript('./GIS/foliumJS.js', function() {
-        //alert('Load was performed.');
-    });
+#print( "AAA " + str( nameFoliumMap ) )
 
-    $(document).ready(function () {
-        addFoliumFunc( {map} );
-    });
+m.get_root().html.add_child(folium.Element("""
+    <script type="text/javascript">     
+
+        $.getScript('./GIS/foliumJS.js', function() {
+                //alert('Load was performed.');
+        })                            
+
+        $(document).ready(function () {
+            //var markers = L.markerClusterGroup(); //L.marker([0, 0] )
+            addFoliumFunc( {map} );
+        });                      
 
     </script>
 """.replace("{map}", nameFoliumMap)
 )
 )
+
+# m.get_root().script.add_child( folium.Element( "addMarker(  )" )) #inizio script
 
 #display( fig, target="foliumMap")
 
@@ -208,7 +327,15 @@ ff = folium.Marker(
 ff.add_to( m )
 """
 display( fig, target="foliumMap")
+#document.getElementById( "foliumMap" ).appendChild( fig ) Error
 
+"""
+m.get_root().header.add_child(folium.JavascriptLink("https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.3/leaflet.markercluster.js"))
+m.get_root().header.add_child(folium.CssLink("https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.3/MarkerCluster.Default.css"))
+m.get_root().header.add_child(folium.CssLink("https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.3/MarkerCluster.Default.min.css"))
+"""
+
+#print( "PRIMA : " + str( document.getElementById( "foliumMap" ).childNodes.length ) )
 
 add_event_listener( document.getElementById("btn_executeQueryUfoDb") , "click", executeQueryUfoDbFromUser)
 add_event_listener( document.getElementById("num_annoAvvistamentoUFO") , "blur", executeQueryUfoDbFromField)
