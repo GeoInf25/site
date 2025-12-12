5441b33d2f9266771fcb03b01c1154d9a3b734a2
GeoInf25 <michelotti002@gmail.com>
2025-12-12 18:16:36 +0100

    Update file_funzSupp.js

--- file_funzSupp.js
+++ file_funzSupp.js
@@ -12,30 +12,5 @@ var schede = null;
 
 var schedaDabaseCliccata = null; 
 
-function gestioneSchede( scheda ) {
-
-	//console.log( "" + scheda )
-
-	for( var i = 0; i < schede.length; i++ ) {
-		var tempNomeScheda = schede[ i ].id; 
-		if( tempNomeScheda == ( "" + scheda ) ) { //vedi nome schede
-			schede[ i ].style.display = "block"; 
-			schede[ i ].style.visibility = "visible";
-		} else {
-			schede[ i ].style.display = "none"; 
-			schede[ i ].style.visibility = "hidden";
-		}
-	}
-	
-	if( scheda == "schedaDatabase" ) {
-		schedaDabaseCliccata = true; 
-		//console.log( "lato JS " + schedaDabaseCliccata )
-	} else {
-		schedaDabaseCliccata = false; 
-	}
-	
-
-}
-
 
 
